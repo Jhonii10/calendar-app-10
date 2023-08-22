eb1a9e98fdfaa96949d084cf983eac1e8c3051f5
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-21 20:06:07 -0500

    error en netlify 3

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,21 @@
-import {  Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../Calendar";
 
 
 export const AppRouter = () => {
 
-
+    const authStatus = 'authenticated';
 
     return (
         <>
            <Routes>
-           
-            
-              <Route path="/auth/*" element={<LoginPage/>}/>
-              <Route path="/" element={<CalendarPage/>}/>
-           
-            {/* <Route path="/*" element={<Navigate to={'/auth/login'}/>}/> */}
+           {
+            (authStatus === 'not-authenticated')
+                ? <Route path="/auth/*" element={<LoginPage/>}/>
+                : <Route path="/*" element={<CalendarPage/>}/>
+           }
+            <Route path="/*" element={<Navigate to={'/auth/login'}/>}/>
             
            </Routes>
         </>
