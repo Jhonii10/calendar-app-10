b3e073a1ef303667b2a39c744f4c1d8d741c8890
Jhonni10 <jhonieripianoscue@gmail.com>
2023-09-02 14:08:11 -0500

    version 1.0 PWA añadido

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import {fileURLToPath} from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
+  resolve:{
+    alias:{
+      '@':fileURLToPath(new URL('./src',import.meta.url))
+    }
+  }
+});
